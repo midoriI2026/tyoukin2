6d9f49babe1ced9f17100614a5e0ae69997db36d
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 16:14:31 +0900

    Add user script for time card and attendance checks

---
+++ tyoukin2.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         ★超勤：タイムカード＆実績終了＆休憩チェック（完成版）
+// @namespace    http://tampermonkey.net/
+// @version      2.3
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=150*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=TimeCardIndex
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/tyoukin2/main/tyoukin2.js?v=1.0
+// @grant        none
+// ==/UserScript==
